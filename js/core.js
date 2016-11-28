00f5cb72bceb2d0860626520e373bcafb559155f
zhangtian3 <zhangtian3@lenovo.com>
2016-11-28 16:46:55 +0800

    fix image upload,add grid create and try to deal with image position

--- js/core.js
+++ js/core.js
@@ -1,17 +1,50 @@
 var storage=window.localStorage,
 	fReader = new FileReader(),
-	img = new Image(),
-	imageName;
+	uploadBtn=document.getElementById('upload'),
+	imageShow=document.getElementById('showUpload'),
+	row=document.getElementById('row'),
+	col=document.getElementById('col'),
+	board=document.getElementById('board');
 
-var uploadBtn=document.getElementById('upload'),
-	imageShow=document.getElementById('showUpload');
-uploadBtn.addEventListener('change',showImg);	
-
-function showImg(e){
-	fReader.readAsDataURL(this.files[0]);
-}
+uploadBtn.addEventListener('change',uploadImg);	
 
 fReader.onload=function(){
 	storage.setItem('img',fReader.result);
+	init();
+}
+
+function uploadImg(e){
+	fReader.readAsDataURL(this.files[0]);
+}
+
+function createGrid(rows,cols,whiteBorder,imgWidth,imgHeight) {
+	board.innerHTML='';
+	var imgSrc=storage.getItem('img');
+	for(var i=0;i<rows;i++){
+		var tmpRow=document.createElement('div');
+		tmpRow.className='row';
+		tmpRow.style.width=imgWidth+cols*whiteBorder+'px';
+		for(var j=0;j<cols;j++){
+			var tmpCol=document.createElement('div');
+			tmpCol.className='col';
+			tmpCol.style.margin=Math.ceil(whiteBorder/2)+'px';
+			tmpCol.style.width=imgWidth/cols+'px';
+			tmpCol.style.height=imgHeight/rows+'px';
+			tmpCol.style.backgroundImage='url('+imgSrc+')';
+			tmpCol.style.backgroundRepeat='no-repeat';
+			tmpCol.style.backgroundPosition=j*(imgWidth/cols)+'px '+i*(imgHeight/rows)+'px';
+			tmpRow.append(tmpCol);
+		}
+		board.append(tmpRow);
+	}
+}
+
+function init(){
 	imageShow.src=storage.getItem('img');
+	var rows=row.value,
+		cols=col.value,
+		imgWidth=imageShow.width,
+		imgHeight=imageShow.height,
+		whiteBorder=Math.floor(imgWidth/30);
+	createGrid(rows,cols,whiteBorder,imgWidth,imgHeight);	
 }
